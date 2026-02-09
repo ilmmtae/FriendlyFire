686988fc330038b53b083c6ff6ac636d247bd7f6
Gleb Shcherbyna <glebshcherbyna784@gmail.com>
2026-02-09 22:05:14 +0200

    add /me endpoint

--- src/api/account.py
+++ src/api/account.py
@@ -2,10 +2,15 @@ from typing import List
 from uuid import UUID
 
 from sqlalchemy.ext.asyncio.session import AsyncSession
-from fastapi import Depends, status, APIRouter, Path
+from fastapi import Depends, status, APIRouter, Path, Header
 
 from src.dependencies.database import RWSessionStub
-from src.schema.account import CreateAccountRequest, AccountResponse, ShortAccountSchema, LoginRequest
+from src.schema.account import (
+    CreateAccountRequest,
+    AccountResponse,
+    ShortAccountSchema,
+    LoginRequest,
+)
 from src.service.account import AccountService
 
 account_router = APIRouter(prefix="/account", tags=["account"])
@@ -30,12 +35,14 @@ async def list_accounts(
 ) -> List[AccountResponse]:
     return await AccountService(db=db).list_accounts()
 
+
 @account_router.get("/{account_id}")
 async def get_account_by_id(
     account_id: UUID = Path(..., description="The ID of the account to retrieve"),
     db: AsyncSession = Depends(RWSessionStub),
 ) -> AccountResponse:
     return await AccountService(db=db).get_account_by_id(account_id)
+
 
 @account_router.delete("/{account_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_account_by_id(
@@ -44,17 +51,27 @@ async def delete_account_by_id(
 ) -> None:
     await AccountService(db=db).delete_account_by_id(account_id=account_id)
 
+
 @account_router.patch("/{account_id}", status_code=status.HTTP_200_OK)
 async def update_account_by_id(
     request: ShortAccountSchema,
     account_id: UUID = Path(..., description="The ID of the account to update"),
     db: AsyncSession = Depends(RWSessionStub),
-)-> AccountResponse:
-    return await AccountService(db=db).update_account_by_id(account_id=account_id, request=request)
+) -> AccountResponse:
+    return await AccountService(db=db).update_account_by_id(
+        account_id=account_id, request=request
+    )
+
 
 @account_router.post("/login")
-async def login(
-    request: LoginRequest,
-    db: AsyncSession = Depends(RWSessionStub)
-):
+async def login(request: LoginRequest, db: AsyncSession = Depends(RWSessionStub)):
     return await AccountService(db).authenticate(request)
+
+
+@account_router.get("/me")
+async def get_my_account(
+    authorization: UUID = Header(..., title="Account id (and JWT later)"),
+    db: AsyncSession = Depends(RWSessionStub),
+) -> AccountResponse:
+    account_id = authorization
+    return await AccountService(db).get_account_by_id(account_id=account_id)
