cec005c72c189722e18355dd8ac85552ecce2bb9
ilmmtae <veronikanazaryk8@gmail.com>
2026-03-01 12:13:33 +0200

    extract bot name to .env

--- src/config/config.py
+++ src/config/config.py
@@ -18,6 +18,7 @@ class Settings(BaseSettings):
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
     BOT_TOKEN: str | None
+    BOT_NAME: str | None
 
     @field_validator("SQLALCHEMY_DATABASE_URI", mode="before")
     @classmethod
